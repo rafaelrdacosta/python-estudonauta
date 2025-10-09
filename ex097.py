76decbc204293efbfaadff245963ad6726b872ac
rafaelrdacosta <rafaelrdacosta@gmail.com>
2025-10-09 05:25:10 -0400

    Continuação estudos 8 OUT 25

--- ex097.py
+++ ex097.py
@@ -1,3 +1,22 @@
 '''
-Exercício
+Exercício 097 - Faça um programa que tenha uma função chamada escreva(), que
+receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho
+adaptável.
+Ex:
+escreva('Olá, Mundo!')
+Saída
+-------------
+ Olá, Mundo!
+-------------
 '''
+
+def escreva(msg):
+    tam = len(msg)
+    print('~' * (tam+4))
+    print(f'{msg:^{tam+4}}')
+    print('~' * (tam + 4))
+
+
+escreva('Gustavo Guanabara')
+escreva('Curso em Python no Youtube')
+escreva('Cev')
---
+++ ex098.py
@@ -0,0 +1,57 @@
+'''
+Exercício 098 - Faça um programa que tenha uma função chamada contador(), que
+receba três parâmetros: início, fim e passo realize a contagem.
+Seu programa tem que realizar três contagens através da função criada:
+a - de 1 até 10, de 1 em 1
+b - de 10 até 0, de 2 em 2
+c - uma contagem personalizada
+'''
+
+from time import sleep
+
+def linha():
+    print('-='*25)
+
+def contagem_personalizada(inicio, fim, passo):
+    if passo < 0:
+        passo = passo * (-1)
+    elif passo == 0:
+        passo = 1
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+    if inicio < fim:
+        for i in range(inicio, fim + 1, passo):
+            print(f'{i} ', end='')
+            sleep(0.5)
+        print('FIM!')
+    elif inicio > fim:
+        for i in range(inicio, fim - 1, passo*(-1)):
+            print(f'{i} ', end='')
+            sleep(0.5)
+        print('FIM!')
+
+def contador():
+    linha()
+    print('Contagem de 1 até 10 de 1 em 1')
+    for i in range(1, 11):
+        print(f'{i} ', end='')
+        sleep(0.5)
+    print('FIM!')
+    linha()
+
+    print('Contagem de 10 até 0 de 2 em 2')
+    for i in range(10, -1, -2):
+        print(f'{i} ', end='')
+        sleep(1)
+    print('FIM!')
+    linha()
+
+    print('Agora é a sua vez de personalizar a contagem!')
+    inicio = int(input('Início: '))
+    fim = int(input('Fim: '))
+    passo = int(input('Passo: '))
+    linha()
+    contagem_personalizada(inicio, fim, passo)
+
+
+#Programa principal
+contador()
